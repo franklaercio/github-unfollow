d7055d665fb7e89cb82a03bfc833c4ae985cd495
Frank Laércio <franklaercio@gmail.com>
2021-10-09 14:53:37 -0300

    Adding close webdriver

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("http://www.python.org")
-assert "Python" in driver.title
+
+driver.close()
