9ee8b54ff1910f867350e3f8cad7617b6b601026
Frank Laércio <franklaercio@gmail.com>
2021-10-10 14:07:55 -0300

    Adding main function

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GITHUB_SECRET = os.getenv('PERSONAL_ACESS_TOKEN')
-USER_NAME = os.getenv('USER')
-BASE_URL = os.getenv('BASE_URL')
-TIME_SLEEP = os.getenv('TIME_SLEEP')
+def main():
+  GITHUB_SECRET = os.getenv('PERSONAL_ACESS_TOKEN')
+  USER_NAME = os.getenv('USER')
+  BASE_URL = os.getenv('BASE_URL')
+  TIME_SLEEP = os.getenv('TIME_SLEEP')
 
-api.unfollow_back(BASE_URL, TIME_SLEEP, USER_NAME, GITHUB_SECRET)
+  api.unfollow_back(BASE_URL, TIME_SLEEP, USER_NAME, GITHUB_SECRET)
+
+if __name__ == "__main__":
+    main()
