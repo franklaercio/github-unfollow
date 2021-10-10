356ef8bb43dfd3b6aa165b94f26f035f421fa7ed
Frank Laércio <franklaercio@gmail.com>
2021-10-10 05:13:23 -0300

    Ajusting code for unfollowing

--- api.py
+++ api.py
@@ -2,28 +2,37 @@ import time
 import json
 import requests
 
-def get_unfollowers(username, credentials):
+def get_unfollowers(username, credentials, page_number):
   base_url = "https://api.github.com/" 
-  url_followers = str(base_url + "users/" + username + "/following?per_page=100&page=1")
+  url_followers = str(base_url + "users/" + username + "/following?per_page=1&page=" + str(page_number))
 
   response_followers = requests.get(url_followers, auth=(username, credentials)).json()
 
-  time.sleep(1)
+  time.sleep(2)
 
   for user in response_followers:
-    login = json.dumps(user["login"])
-
-    print("GET USER " + login)
-
-    url_follow_back = str(base_url + "/users/" + login + "/following/" + username)
+    login = json.dumps(user["login"]).replace("\"", "").replace("\"", "")
+    url_follow_back = base_url + "users/" + login + "/following/" + username
     response_follow_back = requests.get(url_follow_back, auth=(username, credentials))
 
-    print(response_follow_back.status_code)
+    time.sleep(2)
 
-    time.sleep(1)
-
-    if response_follow_back.status_code == 401:
+    if response_follow_back.status_code == 404:
       url_unfollow = str(base_url + "/" + login)
       requests.delete(url_unfollow, auth=(username, credentials))
-      print("UNFOLOW USER " + login + "\n")
-      time.sleep(1)
+      print("Unfollowing " + login + "!\n")
+      time.sleep(2)
+
+  return len(response_followers) < 100 
+
+def unfollow_back(username, credentials):
+  next_page = True
+  page_number = 1
+
+  while next_page:
+    has_next_page = get_unfollowers(username, credentials, page_number)
+
+    if has_next_page:
+      page_number += 1
+    else:
+      next_page = False
--- main.py
+++ main.py
@@ -7,4 +7,4 @@ load_dotenv()
 GITHUB_SECRET = os.getenv('PERSONAL_ACESS_TOKEN')
 USER_NAME = os.getenv('USER')
 
-api.get_unfollowers(USER_NAME, GITHUB_SECRET)
+api.unfollow_back(USER_NAME, GITHUB_SECRET)
